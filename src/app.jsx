e9b5e5f1c9a46a9b4a94904c7d52bd6cf62afd4b
Yagnesh Modh <contact@thespecialcharacter.com>
2023-09-12 10:14:23 +0530

    mountin lcm

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,98 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+// Mounting
+
+// -> constructor
+// -> getDerivedStateFromProps
+// -> render
+
+// Updating
+
+// unMounting
+
+// Error
 
 export default class App extends Component {
   state = {
-    count: 0
+    count: 0,
+    data: null
   }
 
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     greet: `Mr. ${props.name}`,
+  //     count: 0,
+  //   };
+
+  //   // api
+
+  //   // analytics
+  //   // this.increment = this.increment.bind(this);
+  //   // this.decrement = this.decrement.bind(this)
+  // }
+
+  // this will call whenever props or state value change
+  static getDerivedStateFromProps(props, state) {
+    console.log("getDerivedStateFromProps");
+    
+    return {
+      greet: `Mr. ${props.name}`,
+    }
+  }
+
+  // call only once
+  async componentDidMount() { 
+    console.log(document.getElementById("heading"));
+    document.addEventListener("copy", () => {
+      console.log("Coppied");
+    })
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+      const json = await res.json();
+      console.log(json);
+      this.setState({data: json})
+    } catch (error) {
+      
+    }
+
+   }
+
   increment = () => {
-    this.setState(({count}) => ({ count: count + 1 }))
+    this.setState(({ count }) => ({ count: count + 1 }));
   }
 
   decrement = () => {
-    this.setState(({count}) => ({ count: count - 1 }))
-  }
+    this.setState(({ count }) => ({ count: count - 1 }));
+  };
 
   render() {
-    const { count } = this.state
+    console.log("render");
+    const { count, greet, data } = this.state;
+    // const { name } = this.props;
+    // if(count > 3) {
+    //   throw Error("something went wrong...")
+    // }
     return (
       <div>
-        <button type="button" onClick={this.increment}>+</button>
+        {count < 5 && <h1 id='heading'>{greet}</h1>}
+        {data && <h2>{data.title}</h2>}
+        <button type="button" onClick={this.increment}>
+          +
+        </button>
         {count}
-        <button type="button" onClick={this.decrement}>-</button>
+        <button type="button" onClick={this.decrement}>
+          -
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ greet: 'Mrs. Hello' });
+          }}
+        >
+          Change Greet
+        </button>
       </div>
-    )
+    );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
 
-root.render(<App />);
+const g = "Mr."
+
+// App.getDerivedStateFromProps = (props, state) => ({
+//         greet: `${g} ${props.name}`,
+//       })
+
+class Test extends Component {
+  state = {
+    name: 'yagnesh',
+  };
+
+  render() {
+    const { name } = this.state;
+    return (
+      <div>
+        <App name={name} />
+        <button type='button' onClick={() => {
+            this.setState({name: "Rohit"})
+        }}>Change Name</button>
+        <h1>{name}</h1>
+      </div>
+    );
+  }
+}
+
+root.render(<Test />);
